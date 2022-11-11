2e0d360bcce4281e0946a8fe10782a693a74287f
olehvolodko <oleh.volodko.ri.2022@lpnu.ua>
2022-11-11 05:19:07 +0200

    lab6.1 it

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab6.1(it)/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int size = 5;
+			int test[size] = {-12, 1, 4, 6, 0};
+			int S = Sum(test, size);
+			Assert::AreEqual(S, -11);
+		}
+	};
+}
---
+++ lab6.1(it)/Source.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int* a, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		a[i] = Low + rand() % (High - Low + 1);
+}
+void Print(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << a[i];
+	cout << endl;
+}
+int Sum(int* a, const int size)
+{
+	int S = 0;
+	for (int i = 0; i < size; i++)
+		if (a[i] < 0 || a[i] % 2 == 1)
+		{
+			S += a[i];
+		}
+	return S;
+}
+int Substitute(int* a, const int size)
+{
+	int number = 0;
+	for (int i = 0; i < size; i++)
+		if (a[i] < 0 || a[i] % 2 == 1)
+		{
+			a[i] = 0;
+			number++;
+		}
+	return number;
+}
+int main()
+{
+	srand((unsigned)time(NULL)); // ініціалізація генератора випадкових чисел
+	const int n = 24;
+	int a[n];
+	int Low = -50;
+	int High = 15;
+	Create(a, n, Low, High);
+	Print(a, n);
+	cout << "S =" << Sum(a, n) << endl;
+	cout << "Number =" << Substitute(a, n) << endl;
+	Print(a, n);
+	return 0;
+}
+
